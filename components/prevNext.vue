b89dfb30d2f71c06322bb05c1b2d76133a57682e
calpo <calpo22@gmail.com>
2022-11-25 15:54:59 +0900

    133ページまで

---
+++ components/prevNext.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="pnWrapper">
+        <NuxtLink :to="前の記事へのリンク">
+            <img src="~/assets/images/arrow-left.svg" alt="arrow-left" />
+        </NuxtLink>
+        <NuxtLink :to="次の記事へのリンク">
+            <img src="~/assets/images/arrow-right.svg" alt="arrow-right" />
+        </NuxtLink>
+    </div>
+</template>
